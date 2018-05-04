754179c95a5887675f550b818dba1468342b5d96
yanjing05 <yanjing05@baidu.com>
2018-05-05 01:25:19 +0800

    增加对数字字符串的处理

--- Dt/StringUtils.php
+++ Dt/StringUtils.php
@@ -19,12 +19,22 @@ class StringUtils
     public static function checkInputString($value)
     {
         if (is_string($value)) {
-            $value = addslashes(trim($value));
-            // 适用各个 PHP 版本的用法
-            if (get_magic_quotes_gpc()) {
-                $value = stripslashes($value);
+            $value = trim($value);
+            if (is_numeric($value)) {
+                if (false === strpos($value, '.')) {
+                    $value = intval($value);
+                } else {
+                    $value = floatval($value);
+                }
+            } else {
+                $value = addslashes($value);
+                // 适用各个 PHP 版本的用法
+                if (get_magic_quotes_gpc()) {
+                    $value = stripslashes($value);
+                }
             }
         }
+
         return $value;
     }
 

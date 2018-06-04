ed1b194e35913060994d707e2286ab2b52302af9
yanjing05 <yanjing05@baidu.com>
2018-06-04 14:53:16 +0800

    add ServerUtils

---
+++ net/ServerUtils.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file ServerUtils.php
+ * @author strickyan@beishanwen.com
+ * @date 2018/05/01 17:45:23
+ * @brief $_SERVER 工具包
+ */
+
+namespace beishanwen\php\libs\net;
+
+class ServerUtils
+{
+    /**
+     * @brief 获取基础 url，http协议+域名+端口
+     * @author strickyan@beishanwen.com
+     * @return string url
+     */
+    public static function getBaseUrl()
+    {
+        $protocol = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
+        return $protocol . $_SERVER['HTTP_HOST'];
+    }
+}
